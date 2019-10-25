7ed8aec7fdab54afd8ef4f66a4572922abead68f
Kellan Moorse <kmoorse@caltech.edu>
2019-10-25 11:40:15 -0700

    Adding fft w/ preprocessing

--- conv_data.py
+++ conv_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.signal as ss
+import math as m
 
 tt = np.arange(0,10*2e4,10)
 n = len(tt)
@@ -13,17 +14,16 @@ m2 = np.mod(np.cumsum(np.random.random(len(tt)) > 1-1e-3), 2) + np.random.normal
 # Import kernel
 kernels = pd.read_csv('data/emp_kernels.csv')
 tonic = kernels["i1_scaled"]
-tonic = np.hstack((np.zeros(len(tonic)), tonic))
 tonic = tonic[::10]
 tonic = tonic / np.sum(tonic)
 
 # Apply Convolution
-conv1 = np.convolve(m1, tonic, mode='valid')
-conv2 = np.convolve(m2, tonic, mode='valid')
+conv1 = np.convolve(m1, tonic)
+conv2 = np.convolve(m2, tonic)
 
 # Apply Deconvolution
-# deconv1, _ = ss.deconvolve(conv1, tonic)
-# deconv2, _ = ss.deconvolve(conv2, tonic)
+deconv1, _ = ss.deconvolve(conv1, tonic)
+deconv2, _ = ss.deconvolve(conv2, tonic)
 
 # Plot results
 plt.subplot(321)
@@ -32,12 +32,25 @@ plt.plot(tt, m1)
 plt.subplot(323)
 plt.plot(tt, m2)
 
-plt.subplot(325)
-plt.plot(tonic)
+# plt.subplot(325)
+# plt.plot(tonic)
 
-plt.subplot(122)
-plt.plot(m1, m2, '.')
-plt.plot(conv1, conv2, '.')
-# plt.plot(deconv1, deconv2, '.')
+# plt.subplot(122)
+# plt.plot(m1, m2, '.')
+# plt.plot(conv1, conv2, '.')
+
+for scale in np.arange(0.5, 1.75, 0.25):
+
+    stretch = ss.resample(tonic, int(m.floor(len(tonic)*scale)))
+    stretch = stretch / np.sum(stretch)
+    deconv1, _ = ss.deconvolve(conv1, stretch)
+    deconv2, _ = ss.deconvolve(conv2, stretch)
+
+    plt.subplot(325)
+    plt.plot(stretch)
+
+    plt.subplot(122)
+    plt.plot(deconv1, deconv2, '.')
+    plt.xlim([-0.5, 1.5]); plt.ylim([-0.5, 1.5])
 
 plt.show()
---
+++ fourier.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Return the smallest power of 2 greater than or equal to a number
+def expceil(x, a=2):
+
+    temp = np.log(x)/np.log(a)
+    return a**np.ceil(temp)
+
+# Take the fourier transform of the windowed input function
+# Return amplitude, phase, and frequency-spacing
+def fft(ft, t, pad=1, window=np.hanning):
+
+    # Extract sample period
+    if len(t) > 0:
+        dt = np.diff(t[:2])[0]
+        assert np.all(np.diff(t) - dt < dt/1e6)
+    else:
+        dt = t
+
+    if window:
+        ft = window(len(ft))*ft
+
+    # Find power-of-two pad length and apply transform
+    N = int(expceil(len(ft)*pad))
+    ff = np.fft.fft(ft, N)[:N/2]
+    f = np.fft.fftfreq(N, dt)[:N/2]
+
+    # Separate amplitude and phase
+    amp = np.abs(ff)
+    ph = np.angle(ff)
+
+    return (amp, ph, f)
+
+def ifft(ff, f):
+
+    pass
+
+if __name__ == '__main__':
+
+    gauss = lambda x, x0, s: np.exp(-(x-x0)**2/(2*s**2))
+    t = np.arange(0, 10, 0.1)
+    ft = 2*np.sin(2*2*np.pi*t) + np.sin(6*2*np.pi*t)
+    # ft = gauss(t, 5, 2)
+    (amp, ph, f) = fft(ft, t, pad=2)
+
+    plt.subplot(211)
+    plt.plot(t, ft)
+    plt.subplot(212)
+    plt.plot(f, amp)
+    plt.show()
